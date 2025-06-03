3a2b8dd86cbca417f230b5a55106bb2044b154ec
Joedavis02 <209591803+Joedavis02@users.noreply.github.com>
2025-06-03 19:33:22 +0530

    Fix Directory Traversal vulnerability in SupportPortalServlet.java

--- SupportPortalServlet.java
+++ SupportPortalServlet.java
@@ -1,3 +1,2 @@
-String name = request.getParameter("name");
-name = StringEscapeUtils.escapeHtml4(name);  // From Apache Commons Text
-out.println("<h1>Hello " + name + "</h1>");
+String filename = Paths.get(request.getParameter("file")).getFileName().toString(); // prevents traversal
+File file = new File("/uploads/" + filename);
